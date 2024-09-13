eb01846baf88111d683ab20ffe028f54fe446f37
CeloAraujo <celoparaujo99@gmail.com>
2024-09-13 10:35:44 -0300

    aula 02

--- app/src/main/java/com/example/aula05/MainActivity.kt
+++ app/src/main/java/com/example/aula05/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.aula05
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aula05.model.Pessoa
+import java.math.BigDecimal
 
 class MainActivity:Activity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_constraint)
+        setContentView(R.layout.pessoa_activity)
 
-        val nome = findViewById<TextView>(R.id.nomePessoa)
+        /*val nome = findViewById<TextView>(R.id.nome_pessoa)
         nome.setText("Marcelo Araujo")
+        val idade = findViewById<TextView>(R.id.idade_pessoa)
+        idade.setText("24")
+        val profissao = findViewById<TextView>(R.id.profissao_pessoa)
+        profissao.setText("Programador")*/
+
+        val recyclerView= findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter=ListarPessoaAdapter(pessoas = listOf(
+            Pessoa(nome="Marcelo",idade = 24,profissao="Programador",altura= BigDecimal(1.78))
+        ))
     }
 
 }
---
+++ app/src/main/java/com/example/aula05/model/Pessoa.kt
@@ -0,0 +1,5 @@
+package com.example.aula05.model
+
+import java.math.BigDecimal
+
+data class Pessoa(val nome:String, val idade:Int,val profissao:String,val altura:BigDecimal)
